7bba10c69a1702ffe8b03f4b8e9f6c7e426a308b
placeholder <placeholder@example.com>
2022-11-13 20:27:30 -0600

    new file:   Random_Testing.py 	modified:   WavefrontAberrator.py 	modified:   holotorch-lib

---
+++ Random_Testing.py
@@ -0,0 +1,214 @@
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+
+import sys
+import time
+# import warnings
+# import copy
+# import datetime
+# import os
+
+sys.path.append("holotorch-lib/")
+sys.path.append("holotorch-lib/holotorch")
+
+import holotorch.utils.Dimensions as Dimensions
+# from holotorch.utils.Enumerators import *
+# from holotorch.utils.units import * # E.g. to get nm, um, mm etc.
+# from holotorch.Spectra.WavelengthContainer import WavelengthContainer
+from holotorch.Spectra.SpacingContainer import SpacingContainer
+# from holotorch.CGH_Datatypes.ElectricField import ElectricField
+# # from holotorch.Optical_Propagators.ASM_Prop import ASM_Prop
+# # from holotorch.Optical_Components.Thin_Lens import Thin_Lens
+
+from holotorch.utils.Helper_Functions import ft2, ift2, fft2_inplace, ifft2_inplace
+# from holotorch.utils.Field_Utils import get_field_slice
+# from MiscHelperFunctions import getSequentialModelComponentSequence, addSequentialModelOutputHooks, getSequentialModelOutputSequence, plotModelOutputSequence
+
+import holotorch.utils.Memory_Utils as Memory_Utils
+
+################################################################################################################################
+
+use_cuda = True
+gpu_no = 0
+device = torch.device("cuda:"+str(gpu_no) if use_cuda else "cpu")
+
+################################################################################################################################
+
+# Testing some methods
+	# d0 = Dimensions.BTPCHW(n_batch=1, n_time=1, n_pupil=1, n_channel=1, height=1, width=1)
+	# d1 = Dimensions.BTPCHW(n_batch=1, n_time=4, n_pupil=3, n_channel=1, height=5, width=1)
+	# d2 = Dimensions.BTPCHW(n_batch=5, n_time=4, n_pupil=1, n_channel=2, height=5, width=1)
+	# d3 = Dimensions.BTPCHW(n_batch=5, n_time=3, n_pupil=1, n_channel=2, height=5, width=1)
+	# d4 = Dimensions.TCD(n_time=4, n_channel=2, height=2)
+	# d5 = Dimensions.C(n_channel=2)
+	# s0 = SpacingContainer(spacing=53)
+	# s1 = SpacingContainer(spacing=53)
+	# s2 = SpacingContainer(spacing=123)
+	# print(d0.has_compatible_shape(d0))
+	# print(d0.has_compatible_shape(d1))
+	# print(d0.has_compatible_shape(d2))
+	# print(d0.has_compatible_shape(d3))
+	# print(d0.has_compatible_shape(d4))
+	# print(d0.has_compatible_shape(d5))
+	# print(s0.is_equivalent(s0))
+	# print(s0.is_equivalent(s1))
+	# print(s0.is_equivalent(s2))
+	# print(s1.is_equivalent(s1))
+	# print(s1.is_equivalent(s2))
+	# print(s2.is_equivalent(s2))
+
+# Testing FFT shift and row-column 2D DFT algorithm stuff
+	# a = torch.rand(5, 17) + 0j
+	# # a = torch.ones(7, 17) + 0j
+
+	# a2 = torch.zeros_like(a)
+	# for r in range(a.shape[-2]):
+	# 	a2[r,:] = torch.fft.fftshift(torch.fft.fft(torch.fft.fftshift(a[r,:], dim=-1), norm='backward'), dim=-1)
+	# for c in range(a.shape[-1]):
+	# 	a2[:,c] = torch.fft.fftshift(torch.fft.fft(torch.fft.fftshift(a2[:,c], dim=-1), norm='backward'), dim=-1)
+
+	# # for r in range(a.shape[-2]):
+	# # 	a2[r,:] = torch.fft.fft(a[r,:], norm='backward')
+	# # for c in range(a.shape[-1]):
+	# # 	a2[:,c] = torch.fft.fft(a2[:,c])
+
+	# a3 = torch.fft.fftshift(torch.fft.fft2(torch.fft.fftshift(a, dim=(-2,-1)), norm='backward'), dim=(-2,-1))
+
+# Testing FFT memory usage
+	# Memory_Utils.print_cuda_memory_usage(device=device)
+	# a = torch.rand(3036, 4024, dtype=torch.complex64, device=device)
+	# Memory_Utils.print_cuda_memory_usage(device=device)
+	# a = torch.fft.fft2(a)
+	# Memory_Utils.print_cuda_memory_usage(device=device)
+
+# Testing in-place 2D FFT
+	# # x = torch.arange(1, 4*8 + 1).view(4,8) + 0j
+	# x = torch.rand(3, 1, 2, 5, 4, 8, dtype=torch.complex64, device=device)
+	# x_orig = x.clone()
+	# fft2_inplace(x, centerOrigins=False, norm='ortho')
+	# x2 = torch.fft.fft2(x_orig, norm='ortho')
+	# print(x)
+	# print(x2)
+	# print(torch.equal(x, x2))
+
+# Testing memory stuff
+	# Memory_Utils.print_cuda_memory_usage(device=device)
+	# a = torch.rand(4, 1, 1, 2, 5*3036, 5*3036, dtype=torch.complex64, device=device)
+	# Memory_Utils.print_cuda_memory_usage(device=device)
+	# # a = torch.fft.fft2(a)
+	# fft2_inplace(a)
+	# Memory_Utils.print_cuda_memory_usage(device=device)
+
+################################################################################################################################
+
+# Testing to see if in-place FFT results match the results from other fft2 implementations
+def testInplaceFFTCorrectness(device : torch.device, norm = 'backward', inverse_fft_flag = False, holotorch_fft_flag = False, do_print = True, short_print = False, print_mem_usage = True):
+	centerOrigins = holotorch_fft_flag
+	for i in range(10):
+		a = torch.rand(np.random.randint(4)+1, np.random.randint(2)+1, np.random.randint(3)+1, np.random.randint(3)+1, np.random.randint(3036)+1, np.random.randint(3036)+1, dtype=torch.complex64, device=device)
+		a_orig = a.clone()
+
+		if not inverse_fft_flag:
+			fft2_inplace(a, centerOrigins=centerOrigins, norm=norm)
+			if holotorch_fft_flag:
+				a2 = ft2(a_orig, norm=norm)
+			else:
+				a2 = torch.fft.fft2(a_orig, norm=norm)
+		else:
+			ifft2_inplace(a, centerOrigins=centerOrigins, norm=norm)
+			if holotorch_fft_flag:
+				a2 = ift2(a_orig, norm=norm)
+			else:
+				a2 = torch.fft.ifft2(a_orig, norm=norm)
+		
+		err = a - a2
+		
+		if do_print:
+			printTuple = (	float(a.abs().mean()), float(a.abs().max()), float((a.abs()**2).mean().sqrt()),
+							float(err.abs().mean()), float(err.abs().max()), float((err.abs()**2).mean().sqrt()),
+							float((a.abs()**2).mean().sqrt() / (err.abs()**2).mean().sqrt())
+						)
+			if not short_print:
+				printStr = "\tMean Abs\tMax Abs\t\tRMS\na\t%f\t%f\t%f\tsize: " + str(a.shape) + "\nerr\t%f\t%f\t%f\trms ratio: %f\n"
+			else:
+				printStr = "%f\t%f\t%f"
+				printTuple = (printTuple[2], printTuple[5], printTuple[6])
+			print(printStr % printTuple)
+			if not short_print:
+				print()
+		
+		randInds = [np.random.randint(a.shape[0]), np.random.randint(a.shape[1]), np.random.randint(a.shape[2]), np.random.randint(a.shape[3])]
+		a = a[randInds[0], randInds[1], randInds[2], randInds[3], :, :]
+		if not inverse_fft_flag:
+			if holotorch_fft_flag:
+				a3 = ft2(a_orig[randInds[0], randInds[1], randInds[2], randInds[3], :, :], norm=norm)
+			else:
+				a3 = torch.fft.fft2(a_orig[randInds[0], randInds[1], randInds[2], randInds[3], :, :], norm=norm)
+		else:
+			if holotorch_fft_flag:
+				a3 = ift2(a_orig[randInds[0], randInds[1], randInds[2], randInds[3], :, :], norm=norm)
+			else:
+				a3 = torch.fft.ifft2(a_orig[randInds[0], randInds[1], randInds[2], randInds[3], :, :], norm=norm)
+
+		err = a - a3
+
+		if do_print:
+			printStr = "\tMean Abs\tMax Abs\t\tRMS\na\t%f\t%f\t%f\tsize: " + str(a.shape) + "\nerr\t%f\t%f\t%f\trms ratio: %f\n"
+			printTuple = (	float(a.abs().mean()), float(a.abs().max()), float((a.abs()**2).mean().sqrt()),
+							float(err.abs().mean()), float(err.abs().max()), float((err.abs()**2).mean().sqrt()),
+							float((a.abs()**2).mean().sqrt() / (err.abs()**2).mean().sqrt())
+						)
+			if not short_print:
+				printStr = "\tMean Abs\tMax Abs\t\tRMS\na\t%f\t%f\t%f\tsize: " + str(a.shape) + "\nerr\t%f\t%f\t%f\trms ratio: %f\n"
+			else:
+				printStr = "%f\t%f\t%f"
+				printTuple = (printTuple[2], printTuple[5], printTuple[6])
+			print(printStr % printTuple)
+			if not short_print:
+				print()
+
+		if print_mem_usage:
+			Memory_Utils.print_cuda_memory_usage(device=device, printType=2)
+
+		del a
+		del a_orig
+		del a2
+
+################################################################################################################################
+
+# t0 = time.process_time()
+# testInplaceFFTCorrectness(device=device, inverse_fft_flag=False, print_mem_usage=False)
+# elapsed_time = time.process_time() - t0
+# print(elapsed_time)
+
+# Memory_Utils.print_cuda_memory_usage(device=device)
+# a = torch.rand(4, 1, 1, 2, 2*3036, 2*3036, dtype=torch.complex64, device=device)
+# Memory_Utils.print_cuda_memory_usage(device=device)
+# # a = torch.fft.fft2(a)
+# fft2_inplace(a)
+# Memory_Utils.print_cuda_memory_usage(device=device)
+
+# print("Start:")
+# Memory_Utils.print_cuda_memory_usage(device=device, printType=2)
+# print()
+# x = torch.rand(2*3036*2*3036, dtype=torch.complex64, device=device)
+# print("After initializing x:")
+# Memory_Utils.print_cuda_memory_usage(device=device, printType=2)
+# print()
+# x = torch.fft.fftshift(x, dim=-1)
+# print("After first fftshift:")
+# Memory_Utils.print_cuda_memory_usage(device=device, printType=2)
+# print()
+# x = torch.fft.fft(x, dim=-1, norm='ortho')
+# print("After first fft:")
+# Memory_Utils.print_cuda_memory_usage(device=device, printType=2)
+# print()
+# x = torch.fft.fftshift(x, dim=-1)
+# print("After second fftshift")
+# Memory_Utils.print_cuda_memory_usage(device=device, printType=2)
+# print()
+
+################################################################################################################################
+
+pass
--- WavefrontAberrator.py
+++ WavefrontAberrator.py
@@ -1,58 +1,27 @@
-import warnings
-import sys
-
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
 
+import warnings
+import sys
+
 from holotorch.CGH_Datatypes.ElectricField import ElectricField
 from holotorch.Optical_Components.CGH_Component import CGH_Component
 from holotorch.utils.Dimensions import HW, TensorDimension
+from holotorch.Spectra.SpacingContainer import SpacingContainer
 from holotorch.utils.Enumerators import *
 from holotorch.utils.Helper_Functions import conv, applyFilterSpaceDomain, ft2, ift2
 from holotorch.Optical_Components.SimpleMask import SimpleMask
 from holotorch.Optical_Propagators.ASM_Prop import ASM_Prop
 
+################################################################################################################################
 
-class WavefrontAberrator(CGH_Component):
-	def __init__(	self,
-					model : torch.nn.Module,
-					direction_label : str = '',
-					parameterDict : dict = None
-				) -> None:
-		super().__init__()
-		self.model = model
-		self.direction_label = direction_label
-		self.parameterDict = parameterDict
-
-	def forward(self, field : ElectricField) -> ElectricField:
-		return self.model(field)
-
-
+# This class should not be instantiated manually.  Its subclasses should be instantiated instead.
 class WavefrontAberratorGenerator:
-	"""
-	
-	References for modelType = 'LayeredScreens:
-		- "Realistic phase screen model for forward multiple-scattering media" by Mu Qiao and Xin Yuan
-			- NOTE: Did not implement what was done in the paper; referred to explanation of conventional random phase screens.
-		- "Characterization of the angular memory effect of scattered light in biological tissues" by Schott et al.
-	"""
 	def __init__(	self,
-					modelType				: str,	# <--- Options: 'RandomThickness', 'LayeredScreens'
 					resolution				: list or tuple,
 					elementSpacings 		: list or tuple,
-
-					# Parameters for 'RandomThickness' model:
-					meanThickness		: float = None,
-					thicknessVariance	: float = None,
-					correlationLength	: float = None,
-
-					# Parameters for modelType = 'LayeredScreens' model:
-					meanFreePath			: float = None,
-					screenGaussianSigma		: float = None,
-					numLayers				: float = None,
-					reusePropagator			: bool = True,
-
+					generateBidirectional	: bool = False,
 					device					: torch.device = None,
 					gpu_no					: int = 0,
 					use_cuda				: bool = False
@@ -71,14 +40,9 @@ class WavefrontAberratorGenerator:
 		if (not validate2TupleList(elementSpacings)):
 			raise Exception("'elementSpacings' must be a tuple or list with two positive real number elements.")
 
-		if modelType == 'RandomThickness':
-			if (meanThickness is None) or (thicknessVariance is None) or (correlationLength is None):
-				raise Exception("Need to specify 'meanThickness', 'thicknessVariance', and 'correlationLength' when using modelType = 'RandomThickness'.")
-		elif modelType == 'LayeredScreens':
-			if (meanFreePath is None) or (screenGaussianSigma is None) or (numLayers is None):
-				raise Exception("Need to specify 'meanFreePath', 'screenGaussianSigma', and 'numLayers' when using modelType = 'LayeredScreens'.")
-		else:
-			raise Exception("Invalid option for 'modelType'.  Should be either 'RandomThickness' or 'LayeredScreens'.")
+		self.resolution = resolution
+		self.elementSpacings = elementSpacings
+		self.generateBidirectional = generateBidirectional
 
 		if (device != None):
 			self.device = device
@@ -87,93 +51,194 @@ class WavefrontAberratorGenerator:
 			self.gpu_no = gpu_no
 			self.use_cuda = use_cuda
 
-		self.modelType = modelType
-		self.resolution = resolution
-		self.elementSpacings = elementSpacings
-
-		self.meanThickness = meanThickness
-		self.thicknessVariance = thicknessVariance
-		self.correlationLength = correlationLength
-
-		self.meanFreePath = meanFreePath
-		self.screenGaussianSigma = screenGaussianSigma
-		self.numLayers = numLayers
-		self._reusePropagator = reusePropagator
-
 		# This dictionary will be passed to the WavefrontAberrator components that this
 		# WavefrontAberratorGenerator object creates.  That way, the generated WavefrontAberrator
 		# objects---which, unlike this object, may be included in saved models---will still contain
 		# information about the parameters.
 		self._parameterDict =	{
-									'modelType'				: modelType,
-									'resolution'			: resolution,
-									'elementSpacings'		: elementSpacings,
-
-									'meanThickness'			: meanThickness,
-									'thicknessVariance'		: thicknessVariance,
-									'correlationLength'		: correlationLength,
-
-									'meanFreePath'			: meanFreePath,
-									'screenGaussianSigma'	: screenGaussianSigma,
-									'numLayers'				: numLayers,
-									'reusePropagator'		: reusePropagator,
-									
-									'device'				: device,
-									'gpu_no'				: gpu_no,
-									'use_cuda'				: use_cuda
+									'modelType'					: self.modelType,
+									'resolution'				: resolution,
+									'elementSpacings'			: elementSpacings,
+									'generateBidirectional'		: generateBidirectional,
+									'device'					: device,
+									'gpu_no'					: gpu_no,
+									'use_cuda'					: use_cuda,
 								}
-
+								
+		self._initializeFrequencyGrids()
 		self._initializeModel()
 
+	def get_model(self):
+		return self.model
+
+	def get_model_reversed(self):
+		return self.modelReversed
+
+	def _initializeFrequencyGrids(self):
+		self._Kx, self._Ky = WavefrontAberratorGenerator._create_normalized_grid(self.resolution[0], self.resolution[1], self.device)
+		self._Kx = 2*np.pi * self._Kx / self.elementSpacings[0]
+		self._Ky = 2*np.pi * self._Ky / self.elementSpacings[1]
+
+	@classmethod
+	def _generateGaussian(cls, sigma, Kx, Ky, domain='space', device='cpu'):
+		K_transverse = torch.sqrt(Kx**2 + Ky**2)
+		if (domain == 'space'):
+			return torch.exp((-1/2)*(K_transverse**2)/(sigma**2)).to(device=device)
+		elif (domain == 'frequency'):
+			return torch.exp((-1/2)*(sigma**2)*(K_transverse**2)).to(device=device)
+		else:
+			raise Exception("Invalid value for 'domain' argument.")
+
+	@classmethod
+	def _create_normalized_grid(cls, H, W, device):
+			# precompute frequency grid for ASM defocus kernel
+			with torch.no_grad():
+				# Creates the frequency coordinate grid in x and y direction
+				kx = (torch.linspace(0, H - 1, H) - (H // 2)) / H
+				ky = (torch.linspace(0, W - 1, W) - (W // 2)) / W
+				Kx, Ky = torch.meshgrid(kx, ky)
+				return Kx.to(device=device), Ky.to(device=device)
+
+
+
+
+class WavefrontAberrator(CGH_Component):
+	def __init__(	self,
+					direction_label : str = '',
+					parameterDict : dict = None
+				) -> None:
+		super().__init__()
+		self.direction_label = direction_label
+		self.parameterDict = parameterDict
+		self.spacing = SpacingContainer(spacing=list(self.parameterDict['elementSpacings']))
+		self.resolution = self.parameterDict['resolution']
+
+	def _check_field_dimensions_valid_bool(self, field : ElectricField) -> bool:
+		if not field.spacing.is_equivalent(self.spacing):
+			return False
+		if (field.data.shape[-2] != self.resolution[0]) or (field.data.shape[-1] != self.resolution[1]):
+			return False
+		return True
+
+	def _check_field_dimensions_valid(self, field : ElectricField) -> bool:
+		if not self._check_field_dimensions_valid_bool(field):
+			raise Exception("Input field does not match the resolution and/or spacing specified.")
+
+	# Deprecated.  This method is only here to provide backwards compatibility with a previously saved model.
+	def forward(self, field : ElectricField) -> ElectricField:
+		if not hasattr(self, '_warnFlag'):
+			warnings.warn("Should not be using WavefrontAberrator's forward(...) method.")
+			self._warnFlag = True
+		self._check_field_dimensions_valid(field)
+		return self.model(field)
+
+
+
+
+class RandomPhaseScreen(WavefrontAberrator):
+	def __init__(	self,
+					model : torch.nn.Module,
+					direction_label : str = '',
+					parameterDict : dict = None
+				) -> None:
+		super().__init__(
+			direction_label = direction_label,
+			parameterDict = parameterDict
+		)
+		self.model = model
+
+	def forward(self, field : ElectricField) -> ElectricField:
+		self._check_field_dimensions_valid(field)
+		return self.model(field)
+
+
+
+
+class RandomThicknessScreen(WavefrontAberrator):
+	def __init__(	self,
+					thicknesses : torch.Tensor,
+					n_screen : float = None,
+					direction_label : str = '',
+					parameterDict : dict = None
+				) -> None:
+		super().__init__(
+			direction_label = direction_label,
+			parameterDict = parameterDict
+		)
+		self.thicknesses = thicknesses
+		self.n_screen = n_screen
+
+	def forward(self, field : ElectricField) -> ElectricField:
+		self._check_field_dimensions_valid(field)
+
+		# pathLen = 
+
+		return self.model(field)
+
+
+
+
+class RandomThicknessScreenGenerator(WavefrontAberratorGenerator):
+	"""
+	Description:
+		Implements asdasfaasdasdadassdas
+	
+	References:
+		- asdsadasdasdasdasd
+	"""
+	def __init__(	self,
+					resolution				: list or tuple,
+					elementSpacings 		: list or tuple,
+					meanThickness			: float,
+					thicknessVariance		: float,
+					correlationLength		: float,
+					n_screen				: float = None,
+					reuseScreen				: bool = True,
+					generateBidirectional	: bool = False,
+					device					: torch.device = None,
+					gpu_no					: int = 0,
+					use_cuda				: bool = False
+				) -> None:
+
+		self.modelType = 'RandomThicknessScreen'
+
+		super.__init__(
+			resolution				= resolution,
+			elementSpacings			= elementSpacings,
+			generateBidirectional	= generateBidirectional,
+			device					= device,
+			gpu_no					= gpu_no,
+			use_cuda				= use_cuda
+		)
+
+		self.meanThickness = meanThickness
+		self.thicknessVariance = thicknessVariance
+		self.correlationLength = correlationLength
+		self.n_screen = n_screen
+		self.reuseScreen = reuseScreen
+
+		self._parameterDict['meanThickness']		= meanThickness
+		self._parameterDict['thicknessVariance']	= thicknessVariance
+		self._parameterDict['correlationLength']	= correlationLength
+		self._parameterDict['n_screen']				= n_screen
+		self._parameterDict['reuseScreen']			= reuseScreen
+
+
 	def _initializeModel(self):
-		self._initializeFrequencyGrids()
-		if (self.modelType == 'RandomThickness'):
-			self._initializeRandomThicknessModel()
-		elif (self.modelType == 'LayeredScreens'):
-			self._initializeLayeredScreensModel()
-
-	def _initializeLayeredScreensModel(self):
-		if (self._reusePropagator):
-			prop = ASM_Prop(init_distance=self.meanFreePath)
-
-		model = torch.nn.Sequential()
-		for i in range(self.numLayers):
-			model.append(self._generatePhaseScreen(self.screenGaussianSigma))
-			if (self._reusePropagator):
-				model.append(prop)
-			else:
-				model.append(ASM_Prop(init_distance=self.meanFreePath))
-		model.append(self._generatePhaseScreen(self.screenGaussianSigma))
-
-		modelReversed = torch.nn.Sequential()
-		for i in range(len(model) - 1, -1, -1):
-			modelReversed.append(model[i])
-
-		self._modelSequential = model
-		self._modelReversedSequential = modelReversed
-		self.model = WavefrontAberrator(model=model, direction_label='normal', parameterDict=self._parameterDict)
-		self.modelReversed = WavefrontAberrator(model=modelReversed, direction_label='reverse', parameterDict=self._parameterDict)
-
-	def _generatePhaseScreen(self, sigma):
-		screen = SimpleMask(
-								tensor_dimension=HW(self.resolution[0], self.resolution[1]),
-								init_type=INIT_TYPE.ZEROS,
-								mask_model_type=MASK_MODEL_TYPE.COMPLEX,
-								mask_forward_type=MASK_FORWARD_TYPE.MULTIPLICATIVE,
-								mask_opt=False
-							)
-		phaseMask = 2 * np.pi * torch.rand(screen.mask.shape, dtype=torch.float, device=self.device)
-
-		# Want sigma to correspond to width in frequency so setting domain='space'
-		H = WavefrontAberratorGenerator._generateGaussian(sigma, self._Kx, self._Ky, domain='space', device=self.device)
-		
-		h = ift2(H, norm='backward').real
-		phaseMask = applyFilterSpaceDomain(h, phaseMask).real
-		screen.mask = torch.exp(1j*phaseMask).to(device=self.device)
-		return screen
-
-	def _initializeRandomThicknessModel(self):
 		self._initializeRandomHeights()
+		
+		if not self.reuseScreens:
+			self.model = RandomThicknessScreen(thicknesses=self.thicknesses, n_screen=self.n_screen, direction_label='normal', parameterDict=self._parameterDict)
+		else:
+			self.model = RandomThicknessScreen(thicknesses=self.thicknesses, n_screen=self.n_screen, direction_label='both', parameterDict=self._parameterDict)
+
+		if self.generateBidirectional:
+			if not self.reuseScreen:
+				self.modelReversed = RandomThicknessScreen(thicknesses=self.thicknesses, n_screen=self.n_screen, direction_label='reverse', parameterDict=self._parameterDict)
+			else:
+				self.modelReversed = self.model
+		else:
+			self.modelReversed = None
 
 	def _initializeRandomHeights(self):
 		def generateRandomHeightsHelper1(filterSigma, kx, ky, padding, meanThickness, thicknessVariance, device):
@@ -240,33 +305,96 @@ class WavefrontAberratorGenerator:
 
 		return
 
-	def _initializeFrequencyGrids(self):
-		self._Kx, self._Ky = WavefrontAberratorGenerator._create_normalized_grid(self.resolution[0], self.resolution[1], self.device)
-		self._Kx = 2*np.pi * self._Kx / self.elementSpacings[0]
-		self._Ky = 2*np.pi * self._Ky / self.elementSpacings[1]
 
-	@classmethod
-	def _generateGaussian(cls, sigma, Kx, Ky, domain='space', device='cpu'):
-		K_transverse = torch.sqrt(Kx**2 + Ky**2)
-		if (domain == 'space'):
-			return torch.exp((-1/2)*(K_transverse**2)/(sigma**2)).to(device=device)
-		elif (domain == 'frequency'):
-			return torch.exp((-1/2)*(sigma**2)*(K_transverse**2)).to(device=device)
+
+
+class RandomPhaseScreenGenerator(WavefrontAberratorGenerator):
+	"""
+	Description:
+		Implements a random phase screen model.
+	
+	References:
+		- "Realistic phase screen model for forward multiple-scattering media" by Mu Qiao and Xin Yuan
+			- NOTE: Did not implement what was done in the paper; referred to explanation of conventional random phase screens.
+		- "Characterization of the angular memory effect of scattered light in biological tissues" by Schott et al.
+	"""
+	def __init__(	self,
+					resolution				: list or tuple,
+					elementSpacings 		: list or tuple,
+					meanFreePath			: float,
+					screenGaussianSigma		: float,
+					numLayers				: int,
+					reusePropagator			: bool = True,
+					generateBidirectional	: bool = False,
+					device					: torch.device = None,
+					gpu_no					: int = 0,
+					use_cuda				: bool = False
+				) -> None:
+
+		self.modelType = 'RandomPhaseScreen'
+
+		super.__init__(
+			resolution				= resolution,
+			elementSpacings			= elementSpacings,
+			generateBidirectional	= generateBidirectional,
+			device					= device,
+			gpu_no					= gpu_no,
+			use_cuda				= use_cuda
+		)
+
+		self.meanFreePath = meanFreePath
+		self.screenGaussianSigma = screenGaussianSigma
+		self.numLayers = numLayers
+		self.reusePropagator = reusePropagator
+
+		self._parameterDict['meanFreePath']				= meanFreePath
+		self._parameterDict['screenGaussianSigma']		= screenGaussianSigma
+		self._parameterDict['numLayers']				= numLayers
+		self._parameterDict['reusePropagator']			= reusePropagator
+
+
+	def _initializeModel(self):
+		if (self.reusePropagator):
+			prop = ASM_Prop(init_distance=self.meanFreePath)
+
+		model = torch.nn.Sequential()
+		for i in range(self.numLayers):
+			model.append(self._generatePhaseScreen(self.screenGaussianSigma))
+			if (self.reusePropagator):
+				model.append(prop)
+			else:
+				model.append(ASM_Prop(init_distance=self.meanFreePath))
+		model.append(self._generatePhaseScreen(self.screenGaussianSigma))
+
+		self._modelSequential = model
+		self.model = RandomPhaseScreen(model=model, direction_label='normal', parameterDict=self._parameterDict)
+
+		if self.generateBidirectional:
+			modelReversed = torch.nn.Sequential()
+			for i in range(len(model) - 1, -1, -1):
+				modelReversed.append(model[i])
+
+			self._modelReversedSequential = modelReversed
+			self.modelReversed = RandomPhaseScreen(model=modelReversed, direction_label='reverse', parameterDict=self._parameterDict)
 		else:
-			raise Exception("Invalid value for 'domain' argument.")
+			self._modelReversedSequential = None
+			self.modelReversed = None
 
-	@classmethod
-	def _create_normalized_grid(cls, H, W, device):
-			# precompute frequency grid for ASM defocus kernel
-			with torch.no_grad():
-				# Creates the frequency coordinate grid in x and y direction
-				kx = (torch.linspace(0, H - 1, H) - (H // 2)) / H
-				ky = (torch.linspace(0, W - 1, W) - (W // 2)) / W
-				Kx, Ky = torch.meshgrid(kx, ky)
-				return Kx.to(device=device), Ky.to(device=device)
 
-	def get_model(self):
-		return self.model
+	def _generatePhaseScreen(self, sigma):
+		screen = SimpleMask(
+								tensor_dimension=HW(self.resolution[0], self.resolution[1]),
+								init_type=INIT_TYPE.ZEROS,
+								mask_model_type=MASK_MODEL_TYPE.COMPLEX,
+								mask_forward_type=MASK_FORWARD_TYPE.MULTIPLICATIVE,
+								mask_opt=False
+							)
+		phaseMask = 2 * np.pi * torch.rand(screen.mask.shape, dtype=torch.float, device=self.device)
 
-	def get_model_reversed(self):
-		return self.modelReversed
+		# Want sigma to correspond to width in frequency so setting domain='space'
+		H = WavefrontAberratorGenerator._generateGaussian(sigma, self._Kx, self._Ky, domain='space', device=self.device)
+		
+		h = ift2(H, norm='backward').real
+		phaseMask = applyFilterSpaceDomain(h, phaseMask).real
+		screen.mask = torch.exp(1j*phaseMask).to(device=self.device)
+		return screen
